7c90905ea9c7d4d1fecc61a5895736fb6fd73e50
riteshmjain <riteshmjain2898@gmail.com>
2023-01-06 14:49:11 +0530

    Code review changes implemented

--- Java/LCDSAPintegration/com/lcd/sapintegration/util/LCDSAPIntegrationDataConstants.java
+++ Java/LCDSAPintegration/com/lcd/sapintegration/util/LCDSAPIntegrationDataConstants.java
@@ -18,6 +18,15 @@ public class LCDSAPIntegrationDataConstants {
 	public static final String PROPERTY_RESPONSE = "response";
 	public static final String VALUE_SUCCESS = "Success";
 	public static final String RESPONSE_STATUS_CODE_OK = "200";
+	public static final String PROPERTY_ERROR_MESSAGE = "ErrorMessage";
+	public static final String MSG_SCHEDULER_PROCESSING = "Scheduler Processing";
+	public static final String MSG_JSON_FORMAT_VALIDATION_FAILED = "JSON Format Validation Failed";
+	public static final String MSG_JSON_FORMAT_VALIDATION_COMPLETED = "JSON Format Validation Completed";
+	public static final String JPO_LCD_3DXSAP_INTEGRATION_SCHEDULER = "LCD_3DXSAPIntegrationScheduler";
+	public static final String METHOD_REPUSH_FAILED_BOM_COMPONENTS_TO_SAP = "rePushFailedBomComponentsToSAP";
+	public static final String VALUE_PROCESS_STATUS_FLAG_IN_WORK = "In Work";
+	public static final String VALUE_PROCESS_STATUS_FAILED = "Failed";
+	
 	private LCDSAPIntegrationDataConstants() {
 		
 	}
