bac8ff5ef6c3a171708217267434133948f5e0da
Bhavya Ravani <bhavyaravani@gmail.com>
2023-01-19 20:07:34 +0530

    Code Cleaned, Revision added in search, input validation added, Export CSV added

--- Webapp/webapps/LCD_Part_Where_Used/assets/scripts/main.js
+++ Webapp/webapps/LCD_Part_Where_Used/assets/scripts/main.js
@@ -3,6 +3,7 @@ define("LCD/LCD_Part_Where_Used/assets/scripts/main", [
   "LCD/LCDLIB/scripts/vuetify.v2.6.13.min",
   "DS/WAFData/WAFData",
   "DS/PlatformAPI/PlatformAPI",
+  "DS/DataDragAndDrop/DataDragAndDrop",
   "i18n!LCD/LCD_Part_Where_Used/nls/Part_Where_Used_nls",
   "i18n!LCD/LCD_Part_Where_Used/nls/Part_Where_Used_webservicedetails",
   "css!LCD/LCDLIB/styles/vuetify.v2.6.13.min.css",
@@ -20,6 +21,9 @@ define("LCD/LCD_Part_Where_Used/assets/scripts/main", [
   var myWidget = {
     _3dashboardUrl: widget.widgetDomain,
     _3dspaceUrl: "",
+    onRefresh: function () {
+      myWidget.onLoad();
+    },
     onLoad: function () {
       var apps = PlatformAPI.getAllApplicationConfigurations();
       for (var i = 0; i < apps.length; i++) {
@@ -34,76 +38,61 @@ define("LCD/LCD_Part_Where_Used/assets/scripts/main", [
         vuetify: new Vuetify(),
         template: `
         <v-app>
+        <p class="refreshClass">Last refreshed on: {{ date }} {{ time }}</p>
         <v-container>
           <h2 class="title">
             Part - Where Used
           </h2>
-          <v-snackbar v-model="snackbar">
-            {{ snackbartext }}
-    
-            <template v-slot:action="{ attrs }">
-              <v-btn color="red" text v-bind="attrs" @click="snackbar = false">
-                Close
-              </v-btn>
-            </template>
-          </v-snackbar>
           <v-card-title>
-            <span class="h1 me-4">Part:</span>
+            <span class="h1 me-4 partLabel">Part:</span>
     
-            <v-text-field
-              v-model="search"
-              label="Title"
-              single-line
-              hide-details
-              outlined
-              @keydown.enter="sendRequest()"
-              class="rounded-lg"
-            ></v-text-field>
-    
-            <v-btn icons depressed rounded color="white" @click="sendRequest()">
-              <v-icon color="black">
-                mdi-magnify
-              </v-icon>
-            </v-btn>
+            <v-form @submit.prevent="sendRequest()" v-model="formValidity" class="form">
+              <v-text-field
+                v-model="search"
+                :rules="searchRules"
+                label="Title"
+                single-line
+                outlined
+                class="rounded-lg"
+              ></v-text-field>
+
+              <v-btn
+                icons
+                depressed
+                rounded
+                :disabled="!formValidity"
+                color="white"
+                type="submit"
+                class="searchBtn"
+              >
+                <v-icon color="black">
+                  mdi-magnify
+                </v-icon>
+              </v-btn>
+            </v-form>
           </v-card-title>
     
           <v-data-table
             :headers="header"
             :items="filteredItems"
-            :items-per-page="5"
+            :items-per-page="-1"
+            fixed-header
             class="elevation-1 rounded-lg"
             :loading="loadingStatus"
           >
-            <!-- <template v-slot:header="{ headers }">
-              <td v-for="h in headers">
-                <v-menu offset-y :close-on-content-click="false">
-                  <template v-slot:activator="{ on, attrs }">
-                    <v-btn icon v-bind="attrs" v-on="on">
-                      <v-icon small :color="searchLevel ? 'black' : 'white'">
-                        mdi-magnify-expand
-                      </v-icon>
-                    </v-btn>
-                  </template>
-                  <div style="background-color: white; width: 280px">
-                    <v-text-field
-                      v-model="searchLevel"
-                      class="pa-4"
-                      type="text"
-                      label="Enter the search term"
-                      :autofocus="true"
-                    ></v-text-field>
-                    <v-btn
-                      @click="searchLevel = ''"
-                      small
-                      text
-                      color="primary"
-                      class="ml-2 mb-2"
-                      >Clean</v-btn
-                    >
-                  </div>
-                </v-menu>
-              </td>
-            </template> -->
+            <template v-slot:top>
+              <v-btn
+                color="#005685"
+                class="ma-2 exportBtn"
+                depressed
+                @click="exportTableToCsvMethod(header, result)"
+              >
+                EXPORT
+                <v-icon color="white" class="ps-2">
+                  mdi-file-export
+                </v-icon>
+              </v-btn>
+            </template>
     
             <template v-slot:header.level="{ header }">
               {{ header.text }}
@@ -226,16 +215,16 @@ define("LCD/LCD_Part_Where_Used/assets/scripts/main", [
             </template>
     
             <template v-slot:item.nodeMapArr="{ item }">
-              <span v-for="(i,index) in item.nodeMapArr" :key="i.PartId">
+              <span v-if="item.nodeMapArr === 'Not Present'">
+                Not Present
+              </span>
+              <span v-else v-for="(i, index) in item.nodeMapArr" :key="i.PartId">
                 <a target="_blank" :href="i.PartId">
                   {{ i.PartTitle }}
                 </a>
                 <span v-if="item.nodeMapArr.length > index + 1">
                   {{ " → " }}
                 </span>
-              </span>
-              <span v-if="item.nodeMapArr.length == 0">
-                Not Present 
               </span>
             </template>
           </v-data-table>
@@ -244,7 +233,16 @@ define("LCD/LCD_Part_Where_Used/assets/scripts/main", [
             `,
         data() {
           return {
+            date: "",
+            time: "",
+            formValidity: false,
             search: "",
+            searchRules: [
+              (v) => !!v || "Part Title is required",
+              (v) =>
+                v.length >= 13 ||
+                "Part Title must be equal to or greater than 13 characters",
+            ],
             searchLevel: "",
             searchTopNodeName: "",
             searchTopNodeTitle: "",
@@ -252,9 +250,10 @@ define("LCD/LCD_Part_Where_Used/assets/scripts/main", [
             header: Part_Where_Used_nls.headers,
             result: [],
             loadingStatus: false,
-            snackbar: false,
-            snackbartext: "",
           };
+        },
+        mounted() {
+          this.getCurrentTimestamp();
         },
         computed: {
           filteredItems() {
@@ -283,60 +282,58 @@ define("LCD/LCD_Part_Where_Used/assets/scripts/main", [
           },
         },
         methods: {
-          sendRequest() {
-            if (this.search.trim().length > 0) {
-              this.result = [];
-              this.loadingStatus = true;
-              let reqNo = 0;
-              let bomDataArr = widget.getValue("bomData");
-              bomDataArr.forEach((bomData) => {
-                let message = {
-                  partTitle: this.search.trim(), //V21-B01030-00
-                  bomData: bomData,
-                };
-                let _this = this;
-                reqNo += 1;
-                WAFData.authenticatedRequest(
-                  _3dspaceUrl +
-                    Part_Where_Used_webservicedetails.getPartWhereUsedData,
-                  {
-                    method: "POST",
-                    headers: {
-                      "Content-Type": "application/json",
-                    },
-                    data: JSON.stringify(message),
-                    accept: "application/json",
-                    timeout: 200000,
-                    onComplete: (res) => {
-                      let formatedData = _this.formatUrl(JSON.parse(res));
-                      _this.result = _this.result.concat(formatedData);
-                      reqNo -= 1;
-                      if (reqNo === 0) _this.loadingStatus = false;
-                    },
-                    onFailure: (err) => {
-                      console.log(err);
-                      reqNo -= 1;
-                      if (reqNo === 0) _this.loadingStatus = false;
-                    },
-                  }
-                );
-              });
-            } else {
-              this.snackbar = true;
-              this.snackbartext = "Please enter part title";
-            }
+          getCurrentTimestamp() {
+            const date1 = new Date();
+            const yyyy = date1.getFullYear();
+            let mm = date1.getMonth() + 1; // Months start at 0!
+            let dd = date1.getDate();
+            if (dd < 10) dd = "0" + dd;
+            if (mm < 10) mm = "0" + mm;
+            const formattedToday = mm + "/" + dd + "/" + yyyy;
+            // get the date as a string
+            this.date = formattedToday;
+            // get the time as a string
+            this.time = date1.toLocaleTimeString();
           },
-          // getArrows(nodeMapArr, nodeMap) {
-          //   if (nodeMapArr.length > 0) {
-          //     return nodeMap.sPartId !==
-          //       nodeMapArr[nodeMapArr.length - 1].sPartId
-          //       ? " → "
-          //       : "";
-          //   }
-          // },
+          sendRequest() {
+            this.result = [];
+            this.loadingStatus = true;
+            let reqNo = 0;
+            let bomDataArr = widget.getValue("bomData");
+            bomDataArr.forEach((bomData) => {
+              let message = {
+                partTitle: this.search.trim(), //V21-B01030-00
+                bomData: bomData,
+              };
+              let _this = this;
+              reqNo += 1;
+              WAFData.authenticatedRequest(
+                _3dspaceUrl +
+                  Part_Where_Used_webservicedetails.getPartWhereUsedData,
+                {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  data: JSON.stringify(message),
+                  accept: "application/json",
+                  timeout: 200000,
+                  onComplete: (res) => {
+                    let formatedData = _this.formatUrl(JSON.parse(res));
+                    _this.result = _this.result.concat(formatedData);
+                    reqNo -= 1;
+                    if (reqNo === 0) _this.loadingStatus = false;
+                  },
+                  onFailure: (err) => {
+                    console.log(err);
+                    reqNo -= 1;
+                    if (reqNo === 0) _this.loadingStatus = false;
+                  },
+                }
+              );
+            });
+          },
           formatUrl(data) {
-            // let _3dashboardUrl =
-            //   "https://sjc01en3apapd01.corp.lucid.lcl:446/3DDashboard";
             data.forEach((obj) => {
               if (obj.nodeMapArr.length > 0) {
                 obj.nodeMapArr.forEach((nodeMap) => {
@@ -349,6 +346,8 @@ define("LCD/LCD_Part_Where_Used/assets/scripts/main", [
                     '","objectType":"VPMReference","envId":"OnPremise","serviceId":"3DSpace"}]}}';
                   nodeMap.PartId = asIsUrl + encodeURIComponent(urlToEncode);
                 });
+              } else {
+                obj.nodeMapArr = "Not Present";
               }
             });
             return data;
@@ -367,20 +366,48 @@ define("LCD/LCD_Part_Where_Used/assets/scripts/main", [
               .includes(this.searchTopNodeTitle.toLowerCase());
           },
           filterNavigation(item) {
-            // return item.nodeMapArr
-            //   .toLowerCase()
-            //   .includes(this.searchNavigation.toLowerCase());
-
-            let filterData = "Not Present";
-            if (item.nodeMapArr.length > 0) {
-              filterData = item.nodeMapArr.reduce(
+            if (typeof item.nodeMapArr === "string") {
+              return item.nodeMapArr
+                .toLowerCase()
+                .includes(this.searchNavigation.toLowerCase());
+            } else {
+              let filterData = item.nodeMapArr.reduce(
                 (one, two) => (one += two.PartTitle + ","),
                 ""
               );
+              return filterData
+                .toLowerCase()
+                .includes(this.searchNavigation.toLowerCase());
             }
-            return filterData
-              .toLowerCase()
-              .includes(this.searchNavigation.toLowerCase());
+          },
+          exportTableToCsvMethod(headers, rows) {
+            const arrData = [];
+            rows.map((row) => arrData.push(Object.assign({}, row)));
+
+            let csvContent = "data:text/csv;charset=utf-8,";
+
+            arrData.forEach((row) => {
+              if (Array.isArray(row.nodeMapArr)) {
+                let data = row.nodeMapArr
+                  .map((nav) => nav.PartTitle)
+                  .join(" -> ");
+                row.nodeMapArr = data;
+              }
+            });
+
+            csvContent += [
+              headers.map((header) => header.text).join(","),
+              ...arrData.map((item) => Object.values(item).join(",")),
+            ].join("\n");
+
+            const data = encodeURI(csvContent);
+            const link = document.createElement("a");
+            link.setAttribute("href", data);
+            link.setAttribute(
+              "download",
+              "Part Where Used Report " + this.search + ".csv"
+            );
+            link.click();
           },
         },
       });
